7f587c30b53a8e45722daddd3ac9e0bf3da7dc68
Felipe <felipe-pe27@hotmail.com>
2019-02-11 21:51:35 -0200

    Primeiro Exercicio

---
+++ Exercicio.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          float num1, num2, num3;
+          const float peso1 = 0.1f, peso2 = 0.3f, peso3 = 0.6f;
+
+          Console.WriteLine("Digite a Primeira nota");
+          num1 = float.Parse(Console.ReadLine());
+
+          Console.WriteLine("Digite o Segundo Numero");
+          num2 = float.Parse(Console.ReadLine());
+
+          Console.WriteLine("Digite o Terceiro Numero");
+          num3 = float.Parse(Console.ReadLine());
+
+          float media = num1 * peso1 + num2 * peso2 + peso2 * peso3 + peso3;
+
+          Console.WriteLine("Digite o a media Final " + media);
+          
+        }
+    }
+}
